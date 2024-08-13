49fb128748e60386dd5aaaff4d1a4e406c0a37e6
arkain123 <dimabatkabatka14@gmail.com>
2024-08-13 18:26:59 +0300

    created classes Armor, Character, Item, Spell, Weapon

---
+++ Classes/Item.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_Dcharlist
+{
+    internal class Item
+    {
+        private string name = "item";
+        private string description = null;
+        private int cost = 0;
+        private int weight = 0;
+        private int quantity = 1;
+    }
+}
---
+++ Classes/Weapon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_Dcharlist
+{
+    internal class Weapon
+    {
+        private string name = "weapon";
+        private string weapon_class = null;
+        private int cost = 0;
+        private int damage_dice = 0;
+        private string damage_type = null;
+        private int weight = 0;
+        private int alternate_damage = 0;
+        private int range = 0;
+        private bool isUniversal = false;
+        private bool isRanged = false;
+        private bool isTwoHanded = false;
+        private bool isReacheable = false;
+        private bool isLight = false;
+        private bool isThrowable = false;
+        private bool isSpecial = false;
+        private bool isReloading = false;
+        private bool isHeavy = false;
+        private bool isFencing = false;
+    }
+}
